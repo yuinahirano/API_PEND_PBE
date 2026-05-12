eb6e4457920c1dfabdf9dffd30f42b7973b33055
Fernanda Yuina <yuiihiranoo@gmail.com>
2026-05-12 12:36:53 -0300

    :heavy_plus_sign: build: Adicionando o pacote do cors

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
 import routes from './routes/routes.js';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use('/', routes);
 
 app.listen(process.env.SERVER_PORT, ()=> {
