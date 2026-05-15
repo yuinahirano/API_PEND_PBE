f359347bf760480b3942bb077d7a316e1327afb5
BRUNO DAVI NAVARRO <bruno.navarro6@senaisp.edu.br>
2026-05-15 09:26:47 -0300

    :sparkles: feat: Criação do model de Produtos e suas classes

---
+++ BackEnd/src/models/Produtos.js
@@ -0,0 +1,121 @@
+export class Produtos {
+    #id;
+    #nome;
+    #descricao;
+    #preco;
+    #imagem;
+    #qtdEstoque;
+    #idCategoria;
+
+    constructor(pId, pNome, pDescricao, pPreco, pImagem, pQtdEstoque, pIdCategoria) {
+        this.#id = pId;
+        this.nome = pNome;
+        this.descricao = pDescricao;
+        this.preco = pPreco;
+        this.imagem = pImagem;
+        this.qtdEstoque = pQtdEstoque;
+        this.idCategoria = pIdCategoria;
+    }
+
+    get id() {
+        return this.#id;
+    }
+
+    get nome() {
+        return this.#nome;
+    }
+
+    set nome(value) {
+        this.#validarNome(value);
+        this.#nome = value;
+    }
+
+    get descricao() {
+        return this.#descricao;
+    }
+
+    set descricao(value) {
+        this.#validarDescricao(value);
+        this.#descricao = value;
+    }
+
+    get preco() {
+        return this.#preco;
+    }
+
+    set preco(value) {
+        this.#validarPreco(value);
+        this.#preco = value;
+    }
+
+    get imagem() {
+        return this.#imagem;
+    }
+
+    set imagem(value) {
+        this.#validarImagem(value);
+        this.#imagem = value;
+    }
+
+    get qtdEstoque() {
+        return this.#qtdEstoque;
+    }
+
+    set qtdEstoque(value) {
+        this.#validarQtd(value);
+        this.#qtdEstoque = value;
+    }
+
+    get idCategoria() {
+        return this.#idCategoria;
+    }
+
+    set idCategoria(value) {
+        this.#validarIdCategoria(value);
+        this.#idCategoria = value;
+    }
+
+    #validarNome(value) {
+        if (!value || value.trim().length < 3 || value.trim().length > 45) {
+            throw new Error('O campo nome é obrigatório e deve ter entre 3 e 45 caracteres');
+        }
+    }
+
+    #validarDescricao(value) {
+        if (!value || value.trim().length < 3 || value.trim().length > 100) {
+            throw new Error('O campo descrição é obrigatório e deve ter entre 3 e 100 caracteres');
+        }
+    }
+
+    #validarPreco(value) {
+        if (value === undefined || value < 0) {
+            throw new Error('O campo preco é obrigatório e deve ser um número positivo');
+        }
+    }
+
+    #validarQtd(value) {
+        if (value === undefined || value < 0) {
+            throw new Error('O campo quantidade estoque é obrigatório e deve ser um número positivo');
+        }
+    }
+
+    #validarImagem(value) {
+        if (!value || value.trim().length < 3 || value.trim().length > 250) {
+            throw new Error('O campo imagem é obrigatório e deve ter entre 3 e 250 caracteres');
+        }
+    }
+
+    #validarIdCategoria(value) {
+        if (value === undefined || value < 0) {
+            throw new Error('O campo idCategoria é obrigatório e deve ser um número positivo');
+        }
+    }
+
+    static criar(dados) {
+        return new Produtos(null, dados.nome, dados.descricao, dados.preco, dados.imagem, dados.qtdEstoque, dados.idCategoria);
+    }
+
+    static alterar(dados, id) {
+        return new Produtos(id, dados.nome, dados.descricao, dados.preco, dados.imagem, dados.qtdEstoque, dados.idCategoria);
+    }
+}
