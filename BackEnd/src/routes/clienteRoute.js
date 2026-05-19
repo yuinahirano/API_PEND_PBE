c8d44b3dc0cd666681e3e9c06c8cf8ff01f79b2b
Fernanda Yuina <yuiihiranoo@gmail.com>
2026-05-19 08:39:38 -0300

    :sparkles: feat: Adicionado a rota para selecionar todos os clientes

--- BackEnd/src/routes/clienteRoute.js
+++ BackEnd/src/routes/clienteRoute.js
@@ -4,5 +4,6 @@ import clienteController from '../controllers/clienteController.js';
 const clienteRoutes = Router();
 
 clienteRoutes.post('/', clienteController.create);
+clienteRoutes.get('/', clienteController.selecionar);
 
 export default clienteRoutes;
